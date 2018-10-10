fb77a9f8067b135178ebe99c44e6a577701fa70a
deonara447 <deonara447@ed.amdsb.ca>
2018-10-10 14:41:21 -0400

    Add project files.

---
+++ CashRegisterSummative/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace CashRegisterSummative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.McBurgersLabel = new System.Windows.Forms.Label();
+            this.McFriesLabel = new System.Windows.Forms.Label();
+            this.McDrinksLabel = new System.Windows.Forms.Label();
+            this.McBurgersTextBox = new System.Windows.Forms.TextBox();
+            this.McFriesTextBox = new System.Windows.Forms.TextBox();
+            this.McDrinksTextBox = new System.Windows.Forms.TextBox();
+            this.mcTotalButton = new System.Windows.Forms.Button();
+            this.subTotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.blackLineLabel = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.tenderedTextBox = new System.Windows.Forms.TextBox();
+            this.mcChangeButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.mcPrintButton = new System.Windows.Forms.Button();
+            this.newOrderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.875F);
+            this.titleLabel.ForeColor = System.Drawing.Color.Red;
+            this.titleLabel.Location = new System.Drawing.Point(197, 4);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(208, 85);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "MacDoonald\'s";
+            // 
+            // McBurgersLabel
+            // 
+            this.McBurgersLabel.BackColor = System.Drawing.Color.Transparent;
+            this.McBurgersLabel.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.McBurgersLabel.Location = new System.Drawing.Point(1, 71);
+            this.McBurgersLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.McBurgersLabel.Name = "McBurgersLabel";
+            this.McBurgersLabel.Size = new System.Drawing.Size(80, 18);
+            this.McBurgersLabel.TabIndex = 1;
+            this.McBurgersLabel.Text = "McBurgers";
+            // 
+            // McFriesLabel
+            // 
+            this.McFriesLabel.BackColor = System.Drawing.Color.Transparent;
+            this.McFriesLabel.Font = new System.Drawing.Font("Consolas", 10.875F);
+            this.McFriesLabel.Location = new System.Drawing.Point(1, 95);
+            this.McFriesLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.McFriesLabel.Name = "McFriesLabel";
+            this.McFriesLabel.Size = new System.Drawing.Size(80, 18);
+            this.McFriesLabel.TabIndex = 2;
+            this.McFriesLabel.Text = "McFries";
+            // 
+            // McDrinksLabel
+            // 
+            this.McDrinksLabel.BackColor = System.Drawing.Color.Transparent;
+            this.McDrinksLabel.Font = new System.Drawing.Font("Consolas", 10.875F);
+            this.McDrinksLabel.Location = new System.Drawing.Point(1, 119);
+            this.McDrinksLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.McDrinksLabel.Name = "McDrinksLabel";
+            this.McDrinksLabel.Size = new System.Drawing.Size(80, 18);
+            this.McDrinksLabel.TabIndex = 3;
+            this.McDrinksLabel.Text = "McDrinks";
+            // 
+            // McBurgersTextBox
+            // 
+            this.McBurgersTextBox.Location = new System.Drawing.Point(85, 71);
+            this.McBurgersTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.McBurgersTextBox.Name = "McBurgersTextBox";
+            this.McBurgersTextBox.Size = new System.Drawing.Size(52, 20);
+            this.McBurgersTextBox.TabIndex = 4;
+            // 
+            // McFriesTextBox
+            // 
+            this.McFriesTextBox.Location = new System.Drawing.Point(85, 95);
+            this.McFriesTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.McFriesTextBox.Name = "McFriesTextBox";
+            this.McFriesTextBox.Size = new System.Drawing.Size(52, 20);
+            this.McFriesTextBox.TabIndex = 5;
+            // 
+            // McDrinksTextBox
+            // 
+            this.McDrinksTextBox.Location = new System.Drawing.Point(85, 119);
+            this.McDrinksTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.McDrinksTextBox.Name = "McDrinksTextBox";
+            this.McDrinksTextBox.Size = new System.Drawing.Size(52, 20);
+            this.McDrinksTextBox.TabIndex = 6;
+            // 
+            // mcTotalButton
+            // 
+            this.mcTotalButton.BackColor = System.Drawing.Color.Red;
+            this.mcTotalButton.ForeColor = System.Drawing.Color.White;
+            this.mcTotalButton.Location = new System.Drawing.Point(4, 143);
+            this.mcTotalButton.Margin = new System.Windows.Forms.Padding(2);
+            this.mcTotalButton.Name = "mcTotalButton";
+            this.mcTotalButton.Size = new System.Drawing.Size(133, 24);
+            this.mcTotalButton.TabIndex = 7;
+            this.mcTotalButton.Text = "McTotal";
+            this.mcTotalButton.UseVisualStyleBackColor = false;
+            this.mcTotalButton.Click += new System.EventHandler(this.mcTotalButton_Click);
+            // 
+            // subTotalLabel
+            // 
+            this.subTotalLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subTotalLabel.Font = new System.Drawing.Font("Consolas", 10.875F);
+            this.subTotalLabel.Location = new System.Drawing.Point(1, 169);
+            this.subTotalLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.subTotalLabel.Name = "subTotalLabel";
+            this.subTotalLabel.Size = new System.Drawing.Size(145, 18);
+            this.subTotalLabel.TabIndex = 9;
+            this.subTotalLabel.Text = "Sub Total ";
+            this.subTotalLabel.Visible = false;
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.BackColor = System.Drawing.Color.Transparent;
+            this.taxLabel.Font = new System.Drawing.Font("Consolas", 10.875F);
+            this.taxLabel.Location = new System.Drawing.Point(1, 187);
+            this.taxLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(145, 18);
+            this.taxLabel.TabIndex = 10;
+            this.taxLabel.Text = "Tax       ";
+            this.taxLabel.Visible = false;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BackColor = System.Drawing.Color.Transparent;
+            this.totalLabel.Font = new System.Drawing.Font("Consolas", 10.875F);
+            this.totalLabel.Location = new System.Drawing.Point(1, 205);
+            this.totalLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(145, 18);
+            this.totalLabel.TabIndex = 11;
+            this.totalLabel.Text = "Total     ";
+            this.totalLabel.Visible = false;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.BackColor = System.Drawing.Color.Transparent;
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(323, 311);
+            this.errorLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(206, 13);
+            this.errorLabel.TabIndex = 12;
+            this.errorLabel.Text = "Uh oh! You\'ve made a McError! Try again!";
+            this.errorLabel.Visible = false;
+            // 
+            // blackLineLabel
+            // 
+            this.blackLineLabel.BackColor = System.Drawing.Color.Black;
+            this.blackLineLabel.Location = new System.Drawing.Point(1, 223);
+            this.blackLineLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.blackLineLabel.Name = "blackLineLabel";
+            this.blackLineLabel.Size = new System.Drawing.Size(136, 10);
+            this.blackLineLabel.TabIndex = 13;
+            this.blackLineLabel.Visible = false;
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.BackColor = System.Drawing.Color.Transparent;
+            this.tenderedLabel.Font = new System.Drawing.Font("Consolas", 10.875F);
+            this.tenderedLabel.Location = new System.Drawing.Point(1, 239);
+            this.tenderedLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(80, 18);
+            this.tenderedLabel.TabIndex = 14;
+            this.tenderedLabel.Text = "Tendered";
+            this.tenderedLabel.Visible = false;
+            // 
+            // tenderedTextBox
+            // 
+            this.tenderedTextBox.Location = new System.Drawing.Point(85, 239);
+            this.tenderedTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.tenderedTextBox.Name = "tenderedTextBox";
+            this.tenderedTextBox.Size = new System.Drawing.Size(52, 20);
+            this.tenderedTextBox.TabIndex = 15;
+            this.tenderedTextBox.Visible = false;
+            // 
+            // mcChangeButton
+            // 
+            this.mcChangeButton.BackColor = System.Drawing.Color.Red;
+            this.mcChangeButton.ForeColor = System.Drawing.Color.White;
+            this.mcChangeButton.Location = new System.Drawing.Point(4, 263);
+            this.mcChangeButton.Margin = new System.Windows.Forms.Padding(2);
+            this.mcChangeButton.Name = "mcChangeButton";
+            this.mcChangeButton.Size = new System.Drawing.Size(133, 24);
+            this.mcChangeButton.TabIndex = 16;
+            this.mcChangeButton.Text = "McChange";
+            this.mcChangeButton.UseVisualStyleBackColor = false;
+            this.mcChangeButton.Visible = false;
+            this.mcChangeButton.Click += new System.EventHandler(this.mcChangeButton_Click);
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.changeLabel.Font = new System.Drawing.Font("Consolas", 10.875F);
+            this.changeLabel.Location = new System.Drawing.Point(2, 285);
+            this.changeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(144, 18);
+            this.changeLabel.TabIndex = 17;
+            this.changeLabel.Text = "Change    ";
+            this.changeLabel.Visible = false;
+            // 
+            // mcPrintButton
+            // 
+            this.mcPrintButton.BackColor = System.Drawing.Color.Red;
+            this.mcPrintButton.ForeColor = System.Drawing.Color.White;
+            this.mcPrintButton.Location = new System.Drawing.Point(4, 305);
+            this.mcPrintButton.Margin = new System.Windows.Forms.Padding(2);
+            this.mcPrintButton.Name = "mcPrintButton";
+            this.mcPrintButton.Size = new System.Drawing.Size(133, 24);
+            this.mcPrintButton.TabIndex = 18;
+            this.mcPrintButton.Text = "McPrint";
+            this.mcPrintButton.UseVisualStyleBackColor = false;
+            this.mcPrintButton.Visible = false;
+            this.mcPrintButton.Click += new System.EventHandler(this.mcPrintButton_Click);
+            // 
+            // newOrderButton
+            // 
+            this.newOrderButton.BackColor = System.Drawing.Color.Red;
+            this.newOrderButton.ForeColor = System.Drawing.Color.White;
+            this.newOrderButton.Location = new System.Drawing.Point(57, 305);
+            this.newOrderButton.Margin = new System.Windows.Forms.Padding(2);
+            this.newOrderButton.Name = "newOrderButton";
+            this.newOrderButton.Size = new System.Drawing.Size(133, 24);
+            this.newOrderButton.TabIndex = 19;
+            this.newOrderButton.Text = "New Order";
+            this.newOrderButton.UseVisualStyleBackColor = false;
+            this.newOrderButton.Visible = false;
+            this.newOrderButton.Click += new System.EventHandler(this.newOrderButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CashRegisterSummative.Properties.Resources.MacDoonalds;
+            this.ClientSize = new System.Drawing.Size(475, 329);
+            this.Controls.Add(this.newOrderButton);
+            this.Controls.Add(this.mcPrintButton);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.mcChangeButton);
+            this.Controls.Add(this.tenderedTextBox);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.blackLineLabel);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subTotalLabel);
+            this.Controls.Add(this.mcTotalButton);
+            this.Controls.Add(this.McDrinksTextBox);
+            this.Controls.Add(this.McFriesTextBox);
+            this.Controls.Add(this.McBurgersTextBox);
+            this.Controls.Add(this.McDrinksLabel);
+            this.Controls.Add(this.McFriesLabel);
+            this.Controls.Add(this.McBurgersLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "Form1";
+            this.Text = "MacDoonald\'s Cash Register";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label McBurgersLabel;
+        private System.Windows.Forms.Label McFriesLabel;
+        private System.Windows.Forms.Label McDrinksLabel;
+        private System.Windows.Forms.TextBox McBurgersTextBox;
+        private System.Windows.Forms.TextBox McFriesTextBox;
+        private System.Windows.Forms.TextBox McDrinksTextBox;
+        private System.Windows.Forms.Button mcTotalButton;
+        private System.Windows.Forms.Label subTotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.Label blackLineLabel;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox tenderedTextBox;
+        private System.Windows.Forms.Button mcChangeButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Button mcPrintButton;
+        private System.Windows.Forms.Button newOrderButton;
+    }
+}
+
---
+++ CashRegisterSummative/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace CashRegisterSummative
+{
+    public partial class Form1 : Form
+    {
+        const double MCBURGER_COST = 6.79;
+        const double MCFRIES_COST = 1.79;
+        const double MCDRINK_COST = 1.29;
+        const double HST_TAX = 0.13;
+
+        double mcBurgersPurchased;
+        double mcFriesPurchased;
+        double mcDrinksPurchased;
+
+        double subTotalCost;
+        double taxCost;
+        double totalCost;
+
+        double tenderedAmount;
+
+        double changeAmount;
+
+        SoundPlayer beepSoundPlayer = new SoundPlayer(Properties.Resources.beepSound);
+        SoundPlayer errorSoundPlayer = new SoundPlayer(Properties.Resources.errorSound);
+        SoundPlayer printingSoundPlayer = new SoundPlayer(Properties.Resources.printingSound);
+
+        //initialize variables to 0???
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mcTotalButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mcBurgersPurchased = Convert.ToDouble(McBurgersTextBox.Text);
+                mcFriesPurchased = Convert.ToDouble(McFriesTextBox.Text);
+                mcDrinksPurchased = Convert.ToDouble(McDrinksTextBox.Text);
+
+                beepSoundPlayer.Play();
+
+                subTotalLabel.Visible = true;
+                taxLabel.Visible = true;
+                totalLabel.Visible = true;
+                blackLineLabel.Visible = true;
+                tenderedLabel.Visible = true;
+                tenderedTextBox.Visible = true;
+                mcChangeButton.Visible = true;
+
+                errorLabel.Visible = false;
+
+                subTotalCost = mcBurgersPurchased * MCBURGER_COST + mcFriesPurchased * MCFRIES_COST +
+                    mcDrinksPurchased * MCDRINK_COST;
+                taxCost = subTotalCost * HST_TAX;
+                totalCost = subTotalCost + taxCost;
+
+                subTotalLabel.Text = "Sub Total " + subTotalCost.ToString("C");
+                taxLabel.Text = "Tax       " + taxCost.ToString("C");
+                totalLabel.Text = "Total     " + totalCost.ToString("C");
+            }
+            catch
+            {
+                errorLabel.Visible = true;
+
+                errorSoundPlayer.Play();
+            }
+        }
+
+        private void mcChangeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tenderedAmount = Convert.ToDouble(tenderedTextBox.Text);
+                changeAmount = tenderedAmount - totalCost;
+
+                beepSoundPlayer.Play();
+
+                errorLabel.Visible = false;
+                changeLabel.Visible = true;
+                mcPrintButton.Visible = true;
+
+                changeLabel.Text = "Change    " + changeAmount.ToString("C");
+            }
+            catch
+            {
+                errorLabel.Visible = true;
+
+                errorSoundPlayer.Play();
+            }
+        }
+
+        private void mcPrintButton_Click(object sender, EventArgs e)
+        {
+
+            Graphics g = this.CreateGraphics();
+            Pen blackPen = new Pen(Color.Black, 4);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            Font consolasFont = new Font("Consolas", 10, FontStyle.Bold);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+
+            
+
+            for (int i = 0; i < 55; i++)
+            {
+                this.Size = new Size(this.Width - 4, this.Height);
+            }
+
+            printingSoundPlayer.Play();
+            titleLabel.Visible = false;
+
+            McBurgersLabel.Visible = false;
+            McFriesLabel.Visible = false;
+            McDrinksLabel.Visible = false;
+
+            McBurgersTextBox.Visible = false;
+            McFriesTextBox.Visible = false;
+            McDrinksTextBox.Visible = false;
+
+            mcTotalButton.Visible = false;
+            mcChangeButton.Visible = false;
+            mcPrintButton.Visible = false;
+
+            subTotalLabel.Visible = false;
+            taxLabel.Visible = false;
+            totalLabel.Visible = false;
+
+            blackLineLabel.Visible = false;
+
+            tenderedLabel.Visible = false;
+            tenderedTextBox.Visible = false;
+
+            changeLabel.Visible = false;
+
+            //g.Clear(Color.White);
+            g.DrawRectangle(blackPen, 15, 15, 225, 300);
+            g.FillRectangle(whiteBrush, 15, 15, 225, 300);
+
+            g.DrawString("     MacDoonalds Inc." + "\n" + "\n" +
+                "Order Number 02496" + "\n" +
+                "October 15, 2018" + "\n" + "\n" +
+                "McHamburgers  x" + mcBurgersPurchased.ToString("#") + " \t@ 6.79" + "\n" + 
+                "McFries       x" + mcFriesPurchased.ToString("#") + " \t@ 1.79" + "\n" +
+                "McDrinks      x" + mcDrinksPurchased.ToString("#") + " \t@ 1.29" + "\n" + "\n" +
+                "Sub Total        " + subTotalCost.ToString("C") + "\n" +
+                "Tax              " + taxCost.ToString("C") + "\n" + "\n" +
+                "Tendered         " + tenderedAmount.ToString("C") + "\n" +
+                "Change           " + changeAmount.ToString("C") + "\n" + "\n" +
+                "Have a McTastic Day!", 
+                consolasFont, blackBrush, 25, 40);
+
+            Thread.Sleep(5000);
+
+            newOrderButton.Visible = true;
+
+        }
+
+        private void newOrderButton_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+
+
+            for (int i = 0; i < 220; i++)
+            {
+                this.Size = new Size(this.Width + 1, this.Height);
+                this.Refresh();
+
+            }
+
+            titleLabel.Visible = true;
+
+            McBurgersLabel.Visible = true;
+            McFriesLabel.Visible = true;
+            McDrinksLabel.Visible = true;
+
+            McBurgersTextBox.Visible = true;
+            McFriesTextBox.Visible = true;
+            McDrinksTextBox.Visible = true;
+
+            mcTotalButton.Visible = true;
+            newOrderButton.Visible = false;
+
+            McBurgersTextBox.Text = "";
+            McFriesTextBox.Text = "";
+            McDrinksTextBox.Text = "";
+            tenderedTextBox.Text = "";
+
+            this.Refresh();
+        }
+    }
+}
